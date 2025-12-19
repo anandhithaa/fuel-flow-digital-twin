27ba581790a5f23db9beea775b3673d1951a7d57
anandhithaa <ananshaji1@gmail.com>
2025-12-19 14:17:11 +0400

    create Edge class for transport lines

---
+++ model/edge.py
@@ -0,0 +1,6 @@
+class Edge:
+  def __init__ (self, from_node, to_node, capacity, transit_time):
+    self.from_node = from_node
+    self.to_node = to_node
+    self.capacity = capacity
+    self.transit_time = transit_time 
