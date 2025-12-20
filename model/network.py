ef55301d89d2b4a6d8c99225e62d3228b171b487
anandhithaa <ananshaji1@gmail.com>
2025-12-20 12:43:04 +0400

    Create network.py

---
+++ model/network.py
@@ -0,0 +1,10 @@
+class FuelNetwork:
+    def __init__(self):
+        self.nodes = {}
+        self.edges = []
+
+    def add_node(self, node):
+        self.nodes[node.id] = node
+
+    def add_edge(self, edge):
+        self.edges.append(edge)
